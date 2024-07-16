6b8b38045ca61e2d5b873a9e89f9213435b25a49
Dariusssssssss <sablezzubaya@gmail.com>
2024-07-16 21:12:56 +0300

    add: solution before refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports =
+
+    function towelSort(matrix) {
+        let a = []
+        let b = []
+        if (!matrix || !matrix.length) {
+            return []
+        } else {
+            for (i = 0; i < matrix.length; i++) {
+                if (i % 2 !== 0) {
+                    b = matrix[i].reverse()
+                    matrix[i].splice(i, b)
+                }
+            }
+            a = matrix.join(',').split(',').map(Number);
+            return a;
+        }
+
+    }
